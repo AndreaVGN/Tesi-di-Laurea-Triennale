5b4b5a764f1c2f73fc6c1301a8505babb141acd3
AndreaVGN <152275923+AndreaVGN@users.noreply.github.com>
2024-07-02 15:15:20 +0200

    Update CustomIsolationForest_accuracy.py

--- script/CustomIsolationForest_accuracy.py
+++ script/CustomIsolationForest_accuracy.py
@@ -13,19 +13,4 @@ print(CIF_model.score(Dataset.x_test, Dataset.y_test))
 
 y_pred = CIF_model.predict(Dataset.x_test)
 print(y_pred)
-for i in range(0, len(y_pred)):
-    if y_pred[i] == -1:
-        y_pred[i] = 1
-    else:
-        y_pred[i] = 0
-    print(i)
-
-print(y_pred)
-# Creiamo la matrice di confusione
-cm = confusion_matrix(Dataset.y_test, y_pred)
-
-# Visualizziamo la matrice di confusione
-disp = ConfusionMatrixDisplay(confusion_matrix=cm)
-disp.plot(cmap=plt.cm.Blues)
-plt.show()
 
